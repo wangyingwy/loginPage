70992658cde5fca15d84afaaf09b960af5fa9cfe
wangying <549884132@qq.com>
2015-03-21 14:41:22 +0800

    up

--- LoginPage/gen/login/loginpage/R.java
+++ LoginPage/gen/login/loginpage/R.java
@@ -11,35 +11,28 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int android=0x7f020000;
-        public static final int bear=0x7f020001;
-        public static final int bg_editpassword=0x7f020002;
-        public static final int bg_edittext=0x7f020003;
-        public static final int bg_logo=0x7f020004;
-        public static final int bg_password=0x7f020005;
-        public static final int bg_table=0x7f020006;
-        public static final int bg_textview=0x7f020007;
-        public static final int body=0x7f020008;
-        public static final int button=0x7f020009;
-        public static final int button_ring=0x7f02000a;
-        public static final int ic_launcher=0x7f02000b;
-        public static final int imgborder=0x7f02000c;
-        public static final int logo=0x7f02000d;
-        public static final int password=0x7f02000e;
-        public static final int user=0x7f02000f;
+        public static final int bear=0x7f020000;
+        public static final int bg_table=0x7f020001;
+        public static final int body=0x7f020002;
+        public static final int button=0x7f020003;
+        public static final int button_ring=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int logo=0x7f020006;
+        public static final int password=0x7f020007;
+        public static final int user=0x7f020008;
     }
     public static final class id {
-        public static final int big=0x7f050003;
-        public static final int checkBox=0x7f050000;
-        public static final int file=0x7f050001;
-        public static final int login=0x7f050002;
-        public static final int small1=0x7f050004;
-        public static final int small2=0x7f050005;
-        public static final int small3=0x7f050006;
+        public static final int big=0x7f050004;
+        public static final int checkBox=0x7f050001;
+        public static final int file=0x7f050002;
+        public static final int login=0x7f050003;
+        public static final int password=0x7f050000;
+        public static final int small1=0x7f050005;
+        public static final int small2=0x7f050006;
+        public static final int small3=0x7f050007;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int agrrentment=0x7f040002;
